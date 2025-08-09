f3c4857e7f96e1229ec809ac3d91552c594e6531
arthurhfreitass <106278534+arthurhfreitass@users.noreply.github.com>
2025-08-08 22:02:00 -0300

    1.0

--- db.py
+++ db.py
@@ -1,10 +1,6 @@
 import sqlite3
 
-con = sqlite3.connect("tutorial.db")
+con = sqlite3.connect("website.db")
 cur = con.cursor()
 
 
-# cur.execute("CREATE TABLE carrinho(id INTEGER PRIMARY KEY AUTOINCREMENT, id_cliente INTERGE, id_produto INTEGER, nome VARCHAR(250), descricao VARCHAR(500), quantidade INTEGER, preco REAL, FOREIGN KEY (id_produto) REFERENCES produtos (id), FOREIGN KEY (id_cliente) REFERENCES clientes (id))")
-
-
-# cur.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(250) NOT NULL, email TEXT NOT NULL UNIQUE)")
